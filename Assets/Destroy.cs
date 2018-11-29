9d6c9ddfcdfba5e8ef9cc9179465ce6c20bb5ed5
Yagufrix <antonio.sein@g-tec.io>
2018-11-29 09:14:53 -0600

    Prototype

---
+++ Assets/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Bomb")
+		{
+			Destroy(col.gameObject);
+		}
+
+		if(col.gameObject.tag == "Coin")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ Assets/RandomSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour {
+
+	public GameObject coin, bomb;
+
+	public float spawnRate = 2f;
+
+	float nextSpawn = 0f;
+
+	int whatToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextSpawn){
+			whatToSpawn = Random.Range(1,3);
+			Debug.Log (whatToSpawn);
+
+			switch(whatToSpawn){
+				case 1:
+				Instantiate(coin,transform.position,Quaternion.identity);
+				break;
+				case 2:
+				Instantiate(bomb,transform.position,Quaternion.identity);
+				break;
+			}
+			nextSpawn = Time.time + spawnRate;
+		}
+	}
+}
