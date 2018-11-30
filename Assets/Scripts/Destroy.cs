7667b8a844a51f959ac9afb572d0f44b9095605a
Yagufrix <antonio.sein@g-tec.io>
2018-11-30 14:26:04 -0600

    Shaking Camera
    Shaking camera effect fixed, bottle shaking sound, and trigger fixed

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -1,42 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class Destroy : MonoBehaviour {
 
-	Vector3 originalCameraPosition;
-	float shakeAmt = 0;
-	public Camera mainCamera;
-
-	void OnCollisionEnter2D(Collision2D col){
+	public AudioClip bottleShake;
+	void OnTriggerEnter2D(Collider2D col){
 		if (col.gameObject.tag == "Bubble")
 		{
 			Destroy(col.gameObject);
-			shakeAmt = col.relativeVelocity.magnitude * .0025f;
-			InvokeRepeating("CameraShake", 0, .01f);
-			Invoke("StopShaking", 0.3f);
+			CameraShaker.Instance.ShakeOnce(4f,4f,.1f,.1f);
+			AudioSource.PlayClipAtPoint(bottleShake,new Vector3(5,1,2));
 		}
 
 		
-	}
-	void CameraShake()
-		{
-			if(shakeAmt>0)
-			{
-				float quakeAmt = Random.value*shakeAmt*2 - shakeAmt;
-				Vector3 pp = mainCamera.transform.position;
-				pp.y += quakeAmt; // Shakes Y of the camera
-				pp.x += quakeAmt; //Shakes X of the camera
-				mainCamera.transform.position = pp;
-			}
-		}
 
-	void StopShaking()
-	{
-		CancelInvoke("CameraShake");
-		originalCameraPosition.z = -10; // Keeps Z position on -10 in order to keep the main scene on screen
-		originalCameraPosition.y = 0;
-		originalCameraPosition.x = 0;
-	
+		
 	}
+	
+
+	
+
+
 }
