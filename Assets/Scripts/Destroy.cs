894bcdaf8ef7745f665e00febae31102ed60cdd7
Yagufrix <antonio.sein@g-tec.io>
2018-12-03 10:00:27 -0600

    Shaking camera and sprites
    Spawn control added to the destroyer script.

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -6,20 +6,18 @@ using EZCameraShake;
 public class Destroy : MonoBehaviour {
 
 	public AudioClip bottleShake;
+	public GameObject Spawn1,Spawn2,Spawn3;
 	void OnTriggerEnter2D(Collider2D col){
 		if (col.gameObject.tag == "Bubble")
 		{
 			Destroy(col.gameObject);
 			CameraShaker.Instance.ShakeOnce(4f,4f,.1f,.1f);
 			AudioSource.PlayClipAtPoint(bottleShake,new Vector3(5,1,2));	
-			GameObject.Find("SpawnerController").GetComponent<SpawnController>().enabled = true;
+			Spawn1.SetActive(true);
+			Spawn2.SetActive(true);
+			Spawn3.SetActive(true);
+
 		}
-
-		
-
-		
-
-		
 	}
 	
 
