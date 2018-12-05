17fe84dd51311beb58c1b6de783a7f480c3b5d67
Yagufrix <antonio.sein@g-tec.io>
2018-12-05 15:18:27 -0600

    Logo Scene
    Logo Scene added

---
+++ Assets/FadeOut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+
+	public Image splashImage;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSecondsRealtime(2.5f);
+        Fadeout();
+        yield return new WaitForSecondsRealtime(2.5f);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void Fadeout()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
