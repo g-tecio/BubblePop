6b682b7a7ce10328f9809e4254e0f92ff51e2e81
Yagufrix <antonio.sein@g-tec.io>
2018-12-05 10:23:59 -0600

    Fixed restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject gameOverPanel, heart1, heart2, heart3;
+	public GameObject gameOverPanel, heart1, heart2, heart3, Bspawn1, Bspawn2, Bspawn3, Sspawn1, Sspawn2, Sspawn3;
 
-	public void GameOver(){
-		gameOverPanel.SetActive(true);
-	}
+
 
 	public void Restart(){
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		
 	}
 
 	public static int health;
@@ -22,6 +21,13 @@ public class GameManager : MonoBehaviour {
 		heart2.gameObject.SetActive(true);
 		heart3.gameObject.SetActive(true);
 		gameOverPanel.gameObject.SetActive(false);
+		Bspawn1.gameObject.SetActive(true);
+		Bspawn2.gameObject.SetActive(true);
+		Bspawn3.gameObject.SetActive(true);
+		Sspawn1.gameObject.SetActive(false);
+		Sspawn2.gameObject.SetActive(false);
+		Sspawn3.gameObject.SetActive(false);
+		Time.timeScale = 1;
 	}
 	
 	// Update is called once per frame
@@ -34,21 +40,45 @@ public class GameManager : MonoBehaviour {
 			heart1.gameObject.SetActive(true);
 			heart2.gameObject.SetActive(true);
 			heart3.gameObject.SetActive(true);
+			Bspawn1.gameObject.SetActive(true);
+			Bspawn2.gameObject.SetActive(true);
+			Bspawn3.gameObject.SetActive(true);
+			Sspawn1.gameObject.SetActive(false);
+			Sspawn2.gameObject.SetActive(false);
+			Sspawn3.gameObject.SetActive(false);
 			break;
 			case 2:
 			heart1.gameObject.SetActive(true);
 			heart2.gameObject.SetActive(true);
 			heart3.gameObject.SetActive(false);
+			Bspawn1.gameObject.SetActive(true);
+			Bspawn2.gameObject.SetActive(true);
+			Bspawn3.gameObject.SetActive(true);
+			Sspawn1.gameObject.SetActive(true);
+			Sspawn2.gameObject.SetActive(false);
+			Sspawn3.gameObject.SetActive(false);
 			break;
 			case 1:
 			heart1.gameObject.SetActive(true);
 			heart2.gameObject.SetActive(false);
 			heart3.gameObject.SetActive(false);
+			Bspawn1.gameObject.SetActive(true);
+			Bspawn2.gameObject.SetActive(true);
+			Bspawn3.gameObject.SetActive(true);
+			Sspawn1.gameObject.SetActive(true);
+			Sspawn2.gameObject.SetActive(true);
+			Sspawn3.gameObject.SetActive(true);
 			break;
 			case 0:
 			heart1.gameObject.SetActive(false);
 			heart2.gameObject.SetActive(false);
 			heart3.gameObject.SetActive(false);
+			Bspawn1.gameObject.SetActive(false);
+			Bspawn2.gameObject.SetActive(false);
+			Bspawn3.gameObject.SetActive(false);
+			Sspawn1.gameObject.SetActive(false);
+			Sspawn2.gameObject.SetActive(false);
+			Sspawn3.gameObject.SetActive(false);
 			gameOverPanel.gameObject.SetActive(true);
 			Time.timeScale = 0;
 			break;
